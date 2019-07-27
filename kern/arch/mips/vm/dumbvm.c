add04bea135b33ee4c62f0facf0ec8dd75bd6cfd
Ruozhen Gu <gu.gabriel@hotmail.com>
2019-07-27 18:39:46 -0400

    fix ptr usage

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -198,8 +198,8 @@ free_kpages(vaddr_t addr)
 	int targetAddr = addr - MIPS_KSEG0;
 	for(int i = targetAddr; i < frameCount && core_map->inUse[i] != 1; i++){
 		core_map->inUse[i] = 0;
-		if(core_map[i]->containNext[i] != 1) break;
-		else core_map[i]->containNext[i] == 0;
+		if(core_map->containNext[i] != 1) break;
+		else core_map->containNext[i] == 0;
 	}
 	spinlock_release(&spinlock_coremap);
 #else
