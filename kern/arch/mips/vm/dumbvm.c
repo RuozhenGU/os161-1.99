15a8f5238a7a97174ce52bd4868833fb7ce33e4f
Ruozhen Gu <gu.gabriel@hotmail.com>
2019-07-27 22:45:27 -0400

    fix typo

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -209,7 +209,7 @@ free_kpages(vaddr_t addr)
 	}
 	kprintf("freeKpage called\n");
 	spinlock_acquire(&spinlock_coremap);
-	int targetAddr = (addr - cmap->paddr_base) / PAGE_SIZE;
+	int targetAddr = (addr - core_map->baseAddr) / PAGE_SIZE;
 	int i;
 	for(i = targetAddr; i < core_map->size && core_map->inUse[i] == 1 && core_map->containNext[i] == 1; i++){
 		core_map->inUse[i] = 0;
