24e1e3370870bc90523e1a2ad11441e27ad2e73b
Ruozhen Gu <gu.gabriel@hotmail.com>
2019-07-27 21:26:48 -0400

    fix typo

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -150,7 +150,7 @@ getppages(unsigned long npages)
 					if (core_map->inUse[sofar]) break; //ensure the mem loc is still available
 					count++; sofar++;
 				}
-				kprint("here1\n");
+				kprintf("here1\n");
 				if (count < pageRequired) {
 					continue; /*not enough*/
 				} else {
@@ -162,13 +162,13 @@ getppages(unsigned long npages)
 						if (j != pageRequired - 1) core_map->containNext[targetLoc] = 1;
 						else core_map->containNext[targetLoc] = 0; //last element
 				}
-				kprint("here3\n");
+				kprintf("here3\n");
 				addr = i * PAGE_SIZE; //beginning addr grabbed
 				spinlock_release(&stealmem_lock);
 				return addr + core_map->baseAddr;
 				}
 			}
-			kprint("here2\n");
+			kprintf("here2\n");
 			/* else case: continue until free mem is found */
 		} //for loop end
 		/* memory error */
