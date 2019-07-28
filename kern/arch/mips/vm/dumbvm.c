6ede1d99be8ad959a7e648fe2d838408f5ab7800
Ruozhen Gu <gu.gabriel@hotmail.com>
2019-07-27 22:32:22 -0400

    fix free

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -204,19 +204,18 @@ free_kpages(vaddr_t addr)
 {
 #if OPT_A3
 	(void) addr;
-	// if (iscmapCreated == false) {
-	// 	kprintf("no coremap to free\n");
-	// 	return;
-	// }
-	// kprintf("call free\n");
-	// spinlock_acquire(&spinlock_coremap);
-	// int targetAddr = addr - MIPS_KSEG0;
-	// for(int i = targetAddr; i < core_map->size  && core_map->inUse[i] != 1; i++){
-	// 	core_map->inUse[i] = 0;
-	// 	if (core_map->containNext[i] == 0) break;
-	// 	else core_map->containNext[i] = 0;
-	// }
-	// spinlock_release(&spinlock_coremap);
+	if (iscmapCreated == false) {
+		kprintf("no coremap to free\n");
+		return;
+	}
+	spinlock_acquire(&spinlock_coremap);
+	int targetAddr = addr - MIPS_KSEG0;
+	for(int i = targetAddr; i < core_map->size  && core_map->inUse[i] != 1; i++){
+		core_map->inUse[i] = 0;
+		if (core_map->containNext[i] == 0) break;
+		else core_map->containNext[i] = 0;
+	}
+	spinlock_release(&spinlock_coremap);
 #else
 	(void) addr;
 	return;
