36425360f860d5f5ae1e3b09c2ba70d934e0d772
Ruozhen Gu <gu.gabriel@hotmail.com>
2019-07-27 22:05:33 -0400

    add casting

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -173,8 +173,8 @@ getppages(unsigned long npages)
 				kprintf("here4\n");
 				spinlock_release(&stealmem_lock);
 				kprintf("%d\n", addr);
-				kprintf(addr <= addr_hi);
-				kprintf(addr >= addr_lo);
+				kprintf(addr <= (int)addr_hi);
+				kprintf(addr >= (int)addr_lo);
 				return addr;
 				}
 			}
